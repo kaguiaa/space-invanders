86d0a38852bd1272c3a817d513e057b36357506f
Misha <nvishnevskaiaa@gmail.com>
2024-11-23 17:06:56 +0200

    lad04

--- board.hpp
+++ board.hpp
@@ -1,9 +1,12 @@
 #pragma once
+#include <vector>
 
 // Класс для представления игрового поля
 class Board {
     int _width;   // Ширина игрового поля
     int _height;  // Высота игрового поля
+     std::vector<std::vector<int>> _grid;
+
 public:
     // Конструктор с параметрами (по умолчанию 20x20)
     Board(int width = 20, int height = 20);
@@ -13,6 +16,8 @@ public:
 
     // Метод для получения высоты
     int GetHeight() const;
+
+    void Draw();
 };
 
 
--- game_engine.hpp
+++ game_engine.hpp
@@ -6,31 +6,40 @@
 #include "bullet.hpp"
 #include <vector>
 #include <iostream>
-#include <algorithm>
-#include <windows.h>
+#include <memory>  // Для умных указателей
+#include <algorithm>  // Для STL алгоритмов
+
+#ifdef _WIN32
+#include <windows.h>  // Только для Windows
+#endif
 
 // Основной движок игры, объединяющий все элементы (корабль, враги, пули, игровое поле)
 class GameEngine {
-    Ship _ship;                // Корабль игрока
-    std::vector<Enemy> _enemies; // Враги
-    Bullet _bullet;            // Пуля
-    Board _board;              // Игровое поле
-    bool _isGameOver;          // Флаг окончания игры
+    std::unique_ptr<Ship> _ship;    // Умный указатель на корабль
+    std::vector<Enemy> _enemies;   // Враги
+    Bullet _bullet;                // Пуля
+    Board _board;                  // Игровое поле
+    bool _isGameOver;              // Флаг окончания игры
 
 public:
-    // Конструктор движка, инициализирующий врагов и игровое поле
+    
     GameEngine();
-    // Метод инициализации (можно добавить дополнительные настройки)
+
     void Init();
-    // Основной игровой цикл
+
     void Run();
 
 private:
-    // Метод для отрисовки игрового поля
+   
     void Draw();
+
     void Input();
-    // Метод для обработки логики игры
+    
     void Logic();
-    // Метод для обработки окончания игры
+    
     void GameOver();
+    // Обновление игрового состояния
+    void Update();
+    // Отображение игрового состояния
+    void Render();
 };
